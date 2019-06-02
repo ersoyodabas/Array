9e9cd059de3a094db92e639562a089710160024a
Ersoy Odabas <ersoyodabas@outlook.com>
2019-06-02 17:43:39 +0300

    2

---
+++ ConsoleAppArray/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] boys = new string[5] 
+            {
+                "ali",
+                "veli",
+                "ahmet",
+                "mehmet",
+                "kerem"
+            };
+
+            string[] girls= new string[5]
+            {
+                  "ayşe",
+                  "fatma",
+                  "merve",
+                  "betül",
+                  "dilek"
+            };
+
+
+
+            string[,] array = new string[,]
+            {
+                {"cat", "dog","ersoy"},
+                {"bird", "fish","ahmet"},
+            };
+
+            // ... Print out values.
+            Console.WriteLine(array[0, 0]);
+            Console.WriteLine(array[0, 1]);
+            Console.WriteLine(array[0, 2]);
+
+            Console.Read();
+
+
+        }
+    }
+}
